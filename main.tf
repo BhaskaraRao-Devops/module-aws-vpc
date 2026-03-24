71300929b1301c684ba2b3b8b3084463a272b224
Bhaskar <bhaskardevops@gmail.com>
2026-03-24 21:47:01 +0530

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
-    #source = "git::https://github.com/BhaskaraRao-Devops/terraform-aws-vpc.git"
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/BhaskaraRao-Devops/terraform-aws-vpc.git"
     # vpc_cidr = "10.0.0.0/16"
     # project_name = "roboshop"
     # environment = "dev"
